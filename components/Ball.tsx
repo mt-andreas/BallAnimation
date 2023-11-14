1a76872c398db51f27164ebc7b42d6b2ff3b6f21
Andreas <andreas@turncoin.com>
2023-11-14 12:34:24 +0200

    Fix logic and ensure to use the methods provided by reanimated!

--- components/Ball.tsx
+++ components/Ball.tsx
@@ -1,40 +1,37 @@
 import React from 'react';
 import { StyleSheet, View, Button } from 'react-native';
 import { Dimensions } from 'react-native';
-import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import Animated, { SharedValue, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+const screenWidth = Dimensions.get('window').width; 
+const screenHeight = Dimensions.get('window').height;
+
 
 interface BallProps {
-  positionX: number;
-  positionY: number;
+  positionX: SharedValue<number>;
+  positionY: SharedValue<number>;
 }
 
 const Ball: React.FC<BallProps> = ({ positionX, positionY }) => {
-  const screenWidth = Dimensions.get('window').width; 
-  const screenHeight = Dimensions.get('window').height;
-
-  const translateX = useSharedValue(positionX);
-  const translateY = useSharedValue(positionY);
   const [ballSize, setBallSize] = React.useState({state: false, size: screenWidth * 0.1});
   
+  const astyle = useAnimatedStyle(() => {
+    
+    //Do some checks to prevent the ball from going off the screen
+    if (positionX.value <= -screenWidth/2 + ballSize.size) {
+      positionX.value  = -screenWidth/2 + ballSize.size;
+    } else if (positionX.value >= screenWidth/2 - ballSize.size) {
+      positionX.value = screenWidth/2 - ballSize.size;
+    }
 
-  //Do some checks to prevent the ball from going off the screen
-  if (translateX.value <= -screenHeight/2 + ballSize.size) {
-    translateX.value  = -screenHeight/2 + ballSize.size;
-  } else if (translateX.value >= screenHeight/2 - ballSize.size) {
-    translateX.value = screenHeight/2 - ballSize.size;
-  }
-
-  if (translateY.value <= -screenWidth/2 + ballSize.size) {
-    translateY.value = -screenWidth/2 + ballSize.size;;
-  } else if (translateY.value >= screenWidth/2 - ballSize.size) {
-    translateY.value = screenWidth/2 - ballSize.size;
-  }
-
-
-  React.useEffect(() => {
-    translateX.value = withSpring(positionX);
-    translateY.value = withSpring(positionY);
-  }, [positionX, positionY, ballSize]);
+    if (positionY.value <= -screenHeight/2 + ballSize.size) {
+      positionY.value = -screenWidth/2 + ballSize.size;;
+    } else if (positionY.value >= screenHeight/2 - ballSize.size) {
+      positionY.value = screenHeight/2 - ballSize.size;
+    }
+    
+    return {transform: [{ translateX: withSpring(positionX.value) }, { translateY: withSpring(positionY.value) }]}
+  });
 
   const handlePress = () => {
     ballSize.state ? setBallSize({state: false, size: screenWidth * 0.1}) :  setBallSize({state: true, size: screenWidth * 0.3});
@@ -43,16 +40,17 @@ const Ball: React.FC<BallProps> = ({ positionX, positionY }) => {
   return (
     <View style={styles.contents}>
         <Animated.View
-            style={{
+            style={[{
                 position: 'absolute',
                 width: ballSize.size,
                 height: ballSize.size,
                 borderRadius: ballSize.size / 2,
                 backgroundColor: 'blue',
-                transform: [{ translateX: translateY }, { translateY: translateX }],
+                //transform: [{ translateX: translateY }, { translateY: translateX }],
                 maxHeight: screenHeight - ballSize.size,
-                maxWidth: screenWidth - ballSize.size,
-            }}
+                maxWidth: screenWidth - ballSize.size},
+                astyle
+            ]}
         />
         <View style={styles.button}>
             <Button onPress={handlePress} title="Toggle Ball Size" />
--- components/BallMotion.tsx
+++ components/BallMotion.tsx
@@ -5,28 +5,26 @@ import { useSharedValue } from 'react-native-reanimated';
 
 import Ball from './Ball';
 //import Ball2 from './Ball2';
+Gyroscope.setUpdateInterval(16);
+
 
 const BallMotion = () => {
-
-  const [{x,y,z}, setGyroscopeData] = React.useState({ x: 0, y: 0, z: 0 });
   const positionX = useSharedValue(0);
   const positionY = useSharedValue(0);
-  Gyroscope.setUpdateInterval(16);
 
   React.useEffect(() => {
     const subscription = Gyroscope.addListener((data) => {
-      setGyroscopeData(data);
       positionX.value += data.x * 10; // Adjust the multiplier for sensitivity
       positionY.value += data.y * 10; // Adjust the multiplier for sensitivity
 
     });
 
     return () => subscription.remove();
-  }, []);
+  }, [positionX, positionY]);
 
   return (
     <View style={styles.container}>
-      <Ball positionX={positionX.value} positionY={positionY.value} />
+      <Ball positionX={positionY} positionY={positionX} />
       {/*<Ball2 />*/}
 
     </View>
